db5dac7d4cc38b0b5594b0d68b17e01b9cba651d
Hao H <jp97210069.ncue@gmail.com>
2020-04-27 16:46:57 +0800

    Update readFile.m

--- readFile.m
+++ readFile.m
@@ -6,9 +6,13 @@ function file_contents = readFile(filename)
 
 % Load File
 fid = fopen(filename);
+% 當fid非空時(前面fopen成功開啟檔案時)
 if fid
+    % fscanf讀取格式化資料,%c是讀到的資料轉為字元
+    % inf代表讀取全部資料,會轉為行向量的形式
     file_contents = fscanf(fid, '%c', inf);
     fclose(fid);
+% 如果fid為空則進行else處理
 else
     file_contents = '';
     fprintf('Unable to open %s\n', filename);
